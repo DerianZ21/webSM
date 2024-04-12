80ae2c3d6fde74c930081838648c3dd340f1e37f
DerianZ21 <derianzambrnaom@gmail.com>
2024-04-12 16:16:29 -0500

    ultimo

--- src/components/home/Extra.js
+++ src/components/home/Extra.js
@@ -1,6 +1,10 @@
 import React from "react";
 import '../../styles/home/extra.css'
-import fondoExtra from '../../assets/images/nuestras/fondo-inicio.webp'
+import fondoExtra from '../../assets/images/nuestras/fondo-inicio.webp';
+import tecnologia from '../../assets/images/icon/tecnologia.png';
+import grabacion from '../../assets/images/icon/grabacion.png';
+import monitorizacion from '../../assets/images/icon/monitoreo.svg';
+import registros from '../../assets/images/icon/registros.png';
 
 
 
@@ -11,22 +15,22 @@ function Extra() {
                 <img src={fondoExtra} alt=""></img>
             </div>
             <div className="contenedor-info-extra">
-                <div>
+                <div className="content-centrado">
                     <div className="info-extra">
-                        <img src="" className="img-info-extra" alt=""></img>
-                        <p className="text-info-extra"></p>
+                        <img src={tecnologia} className="img-info-extra" alt=""></img>
+                        <p className="text-info-extra">Tecnología</p>
                     </div>
                     <div className="info-extra">
-                        <img src="" className="img-info-extra" alt=""></img>
-                        <p className="text-info-extra"></p>
+                        <img src={grabacion} className="img-info-extra" alt=""></img>
+                        <p className="text-info-extra">Grabación</p>
                     </div>
                     <div className="info-extra">
-                        <img src="" className="img-info-extra" alt=""></img>
-                        <p className="text-info-extra"></p>
+                        <img src={monitorizacion} className="img-info-extra" alt=""></img>
+                        <p className="text-info-extra">Monitorización</p>
                     </div>
                     <div className="info-extra">
-                        <img src="" className="img-info-extra" alt=""></img>
-                        <p className="text-info-extra"></p>
+                        <img src={registros} className="img-info-extra" alt=""></img>
+                        <p className="text-info-extra">Registros</p>
                     </div>
                 </div>
             </div>
