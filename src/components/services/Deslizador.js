d206ebae9ec89f9a656cf7861cdbd567d12782a0
DERIAN ALFREDO ZAMBRANO MONTOYA <derian.zambranom@ug.edu.ec>
2024-04-30 00:05:41 -0500

    imagesnes para deslizador

--- src/components/services/Deslizador.js
+++ src/components/services/Deslizador.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '../../styles/services/deslizador.css'
-import incendios from '../../assets/images/funciones/deteccion-incendios.png'
-import alarma from '../../assets/images/funciones/sistema-alarma.png'
-import cerco from '../../assets/images/funciones/cerco-electrico.png'
-import acceso from '../../assets/images/funciones/control-acceso.png'
-import vehicular from '../../assets/images/funciones/control-vehicular2.webp'
+import incendios from '../../assets/images/funciones/deteccion-incendios.webp'
+import alarma from '../../assets/images/funciones/sistema-alarma.webp'
+import cerco from '../../assets/images/funciones/cerco-electrico.webp'
+import acceso from '../../assets/images/funciones/control-acceso.webp'
+import vehicular from '../../assets/images/funciones/control-vehicular.webp'
 
 function Deslizador() {
 
@@ -41,66 +41,25 @@ function Deslizador() {
     }
   ];
 
-  const [nav1, setNav1] = useState(null);
-  const [nav2, setNav2] = useState(null);
-  let sliderRef1 = useRef(null);
-  let sliderRef2 = useRef(null);
-
-  useEffect(() => {
-    setNav1(sliderRef1);
-    setNav2(sliderRef2);
-  }, []);
-
 
   const settingsCarousel = {
     dots: false,
-    centerPadding: "75px",
+    arrows:true,
     infinite: true,
     centerMode: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    vertical: true,
-    verticalSwiping: true,
-    beforeChange: function (currentSlide, nextSlide) {
-      console.log("before change", currentSlide, nextSlide);
-    },
-    afterChange: function (currentSlide) {
-      console.log("after change", currentSlide);
-    },
-  };
-
-
-  const settingsImg = {
-    arrows: false,
-    dots: false,
-    fade: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    waitForAnimate: false
   };
 
   return (
     <div className='main-deslizador'>
-      <div className='deslizador-carousel'>
-        <Slider className='sliderDeslizador' {...settingsCarousel} asNavFor={nav2} ref={slider => (sliderRef1 = slider)}>
+        <Slider className='sliderDeslizador' {...settingsCarousel} >
           {listPS.map((PS, index) => (
-            <div className='itemPS'>
-              <h3>{PS.id}</h3>
+            <div>
+              <img src={PS.ruta}></img>
             </div>
           ))}
         </Slider>
-      </div>
-      <div className='deslizador-img'>
-        <Slider className='sliderImg' {...settingsImg} asNavFor={nav1} ref={slider => (sliderRef2 = slider)}>
-          {listPS.map((PSimg, index) => (
-            <div className='contenedor-img-deslizador'>
-              <img className="prueba" src={PSimg.ruta} alt={PSimg.descripcion} ></img>
-            </div>
-          ))}
-        </Slider>
-      </div>
     </div>
   );
 }
